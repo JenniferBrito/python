edcf3f15db45efb66ab6394abebaa27bd3e3b754
Jennifer Brito <jennifer.brito3@gmail.com>
2024-10-01 21:07:23 -0300

    Alterações no gráfico de temperaturas máximas e mínimas

--- Projetos/Download de dados/Conceitos/highs_lows.py
+++ Projetos/Download de dados/Conceitos/highs_lows.py
@@ -12,27 +12,41 @@ permite rapidamente a extração de valores desejados.
 """
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+# filename = 'sitka_weather_07-2014.csv'
+# filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-# Obetendo as temperaturas máximas do arquivo
+# Obetendo as datas e as temperaturas máximas e mínimas do arquivo
 
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
-        high = int(row[1])
-        highs.append(high)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'Dados não encontrados')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Faz a plotagem dos dados
 fig = plt.figure(dpi = 128, figsize = (10,6))
-plt.plot(highs, c = 'red')
+plt.plot(dates, highs, c = 'red', alpha =0.5)
+plt.plot(dates, lows, c = 'blue', alpha = 0.5)
+plt.fill_between(dates, highs, lows, facecolor ='blue', alpha = 0.1)
 
 # Formata o gráfico
-
-plt.title('Temperaturas máximas, julho de 2014', fontsize = 24)
+title = 'Temperaturas máximas e mínimas em 2014\nDeath Valley, CA'
+plt.title(title, fontsize = 20)
 plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 plt.ylabel('Temperatura (Fº)', fontsize = 16)
 plt.tick_params(axis='both', which ='major', labelsize = 16)
 
